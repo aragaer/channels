1d0b666a46bf1f2bde4a6b3f4a713d3e3c3c8144
aragaer <aragaer@gmail.com>
2018-11-05 13:29:34 +0300

    Update UnixSocket poller tests

--- tests/unix_sock_test.py
+++ tests/unix_sock_test.py
@@ -10,32 +10,60 @@ from channels.poller import Poller
 from utils import timeout
 
 
-class UnixSocketTest(unittest.TestCase):
+class UnixSocketTest:
 
-    def setUp(self):
+    def prepare(self, *, buffering):
         self._dir = mkdtemp()
         self._sock_file = os.path.join(self._dir, "sock")
         self.addCleanup(shutil.rmtree, self._dir)
         self._server = socket.socket(socket.AF_UNIX)
         self._server.bind(self._sock_file)
-        self._server.listen()
-        self._poller = Poller()
+        self._server.listen(0)
+        self._poller = Poller(buffering=buffering)
         self._poller.add_server(self._server)
         self.addCleanup(self._poller.close_all)
 
-        self._client = socket.socket(socket.AF_UNIX)
-        self._client.connect(self._sock_file)
-        self.addCleanup(self._client.close)
+    def verify(self, data, *expected):
+        client = socket.socket(socket.AF_UNIX)
+        client.connect(self._sock_file)
+        self.addCleanup(client.close)
 
         with timeout(0.1):
             result = self._poller.poll(0.01)
-        self._cl_chan = result[0][0][1]
+        cl_chan = result[0][0][1]
 
-    def test_connect(self):
         with timeout(0.1):
-            self._client.send(b"hello\n")
+            client.send(data)
 
         with timeout(0.1):
             result = self._poller.poll(0.01)
 
-        self.assertEqual(result, [(b"hello\n", self._cl_chan)])
+        self.assertEqual(result,
+                         [(a, cl_chan) for a in expected])
+
+
+class UnixSocketBytesTest(unittest.TestCase, UnixSocketTest):
+
+    def setUp(self):
+        self.prepare(buffering=None)
+
+    def test_all(self):
+        for send in (b'hello', b'hello\n', b'hello\nworld\ntest'):
+            with self.subTest(send=send, expect=send):
+                self.verify(send, send)
+
+
+class UnixSocketLinesTest(unittest.TestCase, UnixSocketTest):
+
+    def setUp(self):
+        self.prepare(buffering='line')
+
+    def test_all(self):
+        tc = {
+            b'hello': (),
+            b'hello\n': (b'hello\n',),
+            b'hello\nworld\ntest': (b'hello\n', b'world\n')
+        }
+        for send, expect in tc.items():
+            with self.subTest(send=send, expect=expect):
+                self.verify(send, *expect)
